818ca4fd3230538eb2154697c45e004307c2e8eb
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-24 02:05:49 +0000

    chore(interface/index): update.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,4 +1,3 @@
-export type { AffixOptions } from'./affix-options.interface';
-export type { NameAffix } from './name-affix.interface';
+export type { NameAdfix } from './name-adfix.interface';
 export type { NameConfiguration } from './name-configuration.interface';
 export type { NameOptions } from './name-options.interface';
